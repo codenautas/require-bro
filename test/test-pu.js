da4f6f10dd6f5d126625e33417a80e357d15118f
Emilio Platzer <emilioplatzer@gmail.com>
2026-05-23 17:52:49 -0300

    saco factor común en el servidor de test para usarlo para pruebas manuales.

--- test/test-pu.js
+++ test/test-pu.js
@@ -1,37 +1,6 @@
 "use strict";
 
-var {Server4Test} = require('server4test');
-
-console.log('Server4Test', Server4Test)
-
-class Server extends Server4Test{
-    directServices(){
-        return super.directServices().concat([{
-            path:'/example',
-            html:`
-<!doctype html>
-<script src='lib/require-bro.js'></script>
-<script src='node_modules/like-ar/like-ar.js'></script>
-<script src='node_modules/best-globals/best-globals.js'></script>
-<script src='node_modules/js-to-html/lib/js-to-html.js'></script>
-<script src='node_modules/big.js/big.js'></script>
-<script src='node_modules/json4all/json4all.js'></script>
-<script src='node_modules/type-store/type-store.js'></script>
-<h1>example</h1>
-<button id=calculate>calculate</button>
-<div id=layout></div>
-<script>
-window.addEventListener('load',function(){
-calculate.onclick=function(){
-    layout.textContent=TypeStore.i18n.messages.en.boolean.true;
-    layout.id='result';
-}
-});
-</script>
-</html>    
-        `}])
-    }
-}
+var {serverDemo} = require('../server/pdemo-server.js');
 
 const puppeteer = require('puppeteer');
 
@@ -56,9 +25,7 @@ describe("interactive ",function(){
     var server;
     before(async function(){
         this.timeout(50000);
-        server = new Server({port:39929, "local-file-repo":{enabled:false, directory:null}});
-        console.log("starting server");
-        await server.start();
+        server = serverDemo;
         browser = await puppeteer.launch({headless, slowMo, args});
         page = await browser.newPage();
         page.on('console', msg => { 
